4a5821ea7a6b42b026d41b427b2ce57702e65a45
HarpLoid <walesii19@gmail.com>
2024-03-07 20:56:47 +0100

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,28 +6,30 @@ A function that retuns a list of integers
 that represents a Pascal's Triangle of n
 """
 
+
 def binomial_coeff(n, k):
     val = 1
     if (k > n - k):
         k = n - k
-    
+
     for i in range(k):
         val = val * (n - i)
         val = val // (i + 1)
-    
+
     return val
 
+
 def pascal_triangle(n):
-    
+
     triangle = []
-    
+
     if n <= 0:
         return triangle
-    
+
     for line in range(n):
         row = []
         for i in range(line + 1):
             row.append(binomial_coeff(line, i))
         triangle.append(row)
-    
+
     return triangle
