26642af790906da04c5e9726b1e36a1fdb5c26be
HarpLoid <walesii19@gmail.com>
2024-03-07 21:07:47 +0100

    refactored function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,29 +7,28 @@ that represents a Pascal's Triangle of n
 """
 
 
-def binomial_coeff(n, k):
-    val = 1
-    if (k > n - k):
-        k = n - k
-
-    for i in range(k):
-        val = val * (n - i)
-        val = val // (i + 1)
-
-    return val
-
-
 def pascal_triangle(n):
+    """Pascal Triangle array
+
+    Args:
+        n (int): number of rows
+
+    Returns:
+        array: pascal triangle array
+    """
 
     triangle = []
 
     if n <= 0:
         return triangle
 
-    for line in range(n):
-        row = []
-        for i in range(line + 1):
-            row.append(binomial_coeff(line, i))
+    triangle.append([1])
+    for line in range(1, n):
+        row = [1]
+        for i in range(1, line):
+            val = triangle[line - 1][i - 1] + triangle[line - 1][i]
+            row.append(val)
+        row.append(1)
         triangle.append(row)
 
     return triangle
