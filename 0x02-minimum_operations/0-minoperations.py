d597aaa6001017d391ec9991326ad93d2e6f812e
HarpLoid <walesii19@gmail.com>
2024-03-21 22:58:53 +0100

    task 0, interview question minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 
+Module - Minimum Operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """ 
+    calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    """
+    next = 'H'
+    body = 'H'
+    ops = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            ops += 2
+            next = body
+            body += body
+        else:
+            ops += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return ops
