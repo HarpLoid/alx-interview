bc5a995b320a7a2f0011b079586916fd75f755c0
HarpLoid <walesii19@gmail.com>
2024-04-27 04:55:25 +0100

    rotate 2d matrix interview

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module - Rotate Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotates a 2d matrix in place
+    """
+    n = len(matrix)
+    
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
