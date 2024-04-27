2e291cfd10769f70a73e97286d2c45cb1d9cba91
HarpLoid <walesii19@gmail.com>
2024-04-27 05:18:29 +0100

    style: pycodestyle fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,10 +9,10 @@ def rotate_2d_matrix(matrix):
     rotates a 2d matrix in place
     """
     n = len(matrix)
-    
+
     for i in range(n):
         for j in range(i, n):
             matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-    
+
     for i in range(n):
         matrix[i] = matrix[i][::-1]
